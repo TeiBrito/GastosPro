1f2d583158dffa2ce803ae8c6348983b0ded7dde
TeiBrito <Teibritohdez@gmail.com>
2026-04-07 23:27:22 +0100

    Updated Service Worker version to clear cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gastopro-v1';
+const CACHE_NAME = 'gastopro-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -16,7 +16,22 @@ self.addEventListener('install', (event) => {
     );
 });
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((cacheName) => {
+                    return cacheName !== CACHE_NAME;
+                }).map((cacheName) => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', (event) => {
+
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
